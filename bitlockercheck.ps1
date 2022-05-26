c3cd7b5216354521df9e560bd66ac62bc1e526f6
Kashimareiko <mackenzie.fyfe@icloud.com>
2022-05-26 15:08:59 +0100

    Variable chamge 2
    - Completely removed all Operating System variables as they are not really needed.
    
    - Added more bitlocker code.

--- bitlockercheck.ps1
+++ bitlockercheck.ps1
@@ -1,13 +1,17 @@
-$winver = (Get-WmiObject -class Win32_OperatingSystem).Caption
 $powershellversion = $PSVersionTable.PSVersion.Major
-
-if ($winver -ne ($os1, $os2, $os3) -contains $winver){
-    Write-Host "Bitlocker not supported on $winver"
-    Exit 0
-}
 
 if ($powershellversion -lt 4){
     Write-Host "Upgrade Powershell Version"
     Exit 1010
 }
 else
+{
+
+$bitlockerkey = (Get-BitLockerVolume -MountPoint C).KeyProtector.RecoveryPassword
+$pcsystemtype = (Get-WmiObject -Class Win32_ComputerSystem).PCSystemType
+if ($pcsystemtype -eq "2"){
+$setsystemtype = "Laptop"
+}
+else {
+$setsystemtype = "Desktop"
+}
